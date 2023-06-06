1edd7538ba6efc4614dbf44378770e80fd0dd313
Ozodbek3531 <ozodbek3531@gmail.com>
2023-06-06 11:41:04 +0500

    navbar

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -2,7 +2,11 @@ import React from 'react'
 
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <div className='navbar'>
+      <div className="container navbar__container">
+        <h2>navbar</h2>
+      </div>
+    </div>
   )
 }
 
