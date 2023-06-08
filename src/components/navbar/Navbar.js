18aa13a1bb318714dac9822d454243fb0c645846
Ozodbek3531 <ozodbek3531@gmail.com>
2023-06-08 10:02:24 +0500

    navbar

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,10 +1,19 @@
 import React from 'react'
+import "./Navbar.css"
+import {Link} from "react-router-dom"
+
 
 function Navbar() {
   return (
     <div className='navbar'>
       <div className="container navbar__container">
-        <h2>navbar</h2>
+        <Link to={"/"}>Ozodbek</Link>
+        <ul className='navbar__collection'>
+          <Link to={"/care-teeth"}>care-teeth</Link>
+          <Link to={"/about"}>about</Link>
+          <Link to={"/service"}>service</Link>
+
+        </ul> 
       </div>
     </div>
   )
