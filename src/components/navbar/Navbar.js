052a0312868ef63da750bac541f6137b1e2fb657
AbbosxojaMarufxojayev <abbosxojamarufxojayev07@gmail.com>
2023-06-13 10:01:30 +0500

    navbar media

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./Navbar.css"
 import { NavLink, useLocation } from "react-router-dom"
 import { AiOutlineBars } from "react-icons/ai"
@@ -9,6 +9,7 @@ import navLogo from "../../assets/navbar/navbarLogo.png"
 
 function Navbar() {
   const {pathname} = useLocation()
+  const [navToggle, setNavToggle] = useState(false)
   if(pathname === "/admin" || pathname === "/login"){
     return <></>
   }
@@ -20,27 +21,16 @@ function Navbar() {
           <NavLink to={"/"}><img src={navLogo} alt="" /></NavLink>
         </div>
         <div className="navbar__collection">
-          <ul className="nav__collection">
+          <ul className={navToggle ? " nav__collection activeNav " : "nav__collection"}>
             <li className="nav__routes"><NavLink to={"/"}>Asosiy sahifa</NavLink></li>
             <li className="nav__routes"><NavLink to={"/care-teeth"}>Tish g'amxo'rligi</NavLink></li>
             <li className="nav__routes"><NavLink to={"/about"}>Biz haqimizda</NavLink></li>
             <li className="nav__routes"><NavLink to={"/service"}>Xizmatlar</NavLink></li>
             <li className="nav__routes"><NavLink to={"/login"}>Login</NavLink></li>
-            <li className="nav__routes"><AiOutlineBars className='navbar__bars'/></li>
+            <li className="siderbar__links"><GrFormClose onClick={()=> setNavToggle(false)} className='sidebar__close'/></li>
           </ul>
+            <span className="nav__routes"><AiOutlineBars onClick={() => setNavToggle(!navToggle)} className='navbar__bars'/></span>
         </div>
-        <div className="sidebar__navbar show__sidebar">
-            <div className="sidebar__collection">
-              <ul className="sidebar__routes">
-                <li className="siderbar__links"><GrFormClose className='sidebar__close'/></li>
-                <li className="sidebar__links"><NavLink to={"/"}>Asosiy sahifa</NavLink></li>
-                <li className="sidebar__links"><NavLink to={"/care-teeth"}>Tish g'amxo'rligi</NavLink></li>
-                <li className="sidebar__links"><NavLink to={"/about"}>Biz haqimizda</NavLink></li>
-                <li className="sidebar__links"><NavLink to={"/service"}>Xizmatlar</NavLink></li>
-                <li className="sidebar__links"><NavLink to={"/login"}>Login</NavLink></li>
-              </ul>
-            </div>
-       </div>
        </div>
     </div>
   )
