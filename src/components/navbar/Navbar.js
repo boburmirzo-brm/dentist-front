e320db7443b83d19e23751e5840668306ca38577
AbbosxojaMarufxojayev <abbosxojamarufxojayev07@gmail.com>
2023-06-14 18:11:33 +0500

    navbar media

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import "./Navbar.css"
 import { NavLink, useLocation } from "react-router-dom"
 import { AiOutlineBars } from "react-icons/ai"
-import { GrFormClose } from "react-icons/gr"
+import { IoMdClose } from "react-icons/io"
 import navLogo from "../../assets/navbar/navbarLogo.png"
 
 
@@ -23,12 +23,17 @@ function Navbar() {
         <div className="navbar__collection">
           <ul className={navToggle ? " nav__collection activeNav " : "nav__collection"}>
             <li onClick={()=> setNavToggle(false)} className="nav__routes"><NavLink to={"/"}>Asosiy sahifa</NavLink></li>
+            <span className='cutter__navbar'>|</span>
             <li onClick={()=> setNavToggle(false)} className="nav__routes"><NavLink to={"/care-teeth"}>Tish g'amxo'rligi</NavLink></li>
+            <span className='cutter__navbar'>|</span>
             <li onClick={()=> setNavToggle(false)} className="nav__routes"><NavLink to={"/about"}>Biz haqimizda</NavLink></li>
+            <span className='cutter__navbar'>|</span>
             <li onClick={()=> setNavToggle(false)} className="nav__routes"><NavLink to={"/service"}>Xizmatlar</NavLink></li>
+            <span className='cutter__navbar'>|</span>
             <li onClick={()=> setNavToggle(false)} className="nav__routes"><NavLink to={"/after"}>Ishlarimiz</NavLink></li>
+            <span className='cutter__navbar'>|</span>
             <li onClick={()=> setNavToggle(false)} className="nav__routes"><NavLink to={"/login"}>Login</NavLink></li>
-            <li onClick={()=> setNavToggle(false)} className="siderbar__links"><GrFormClose onClick={()=> setNavToggle(false)} className='sidebar__close'/></li>
+            <li onClick={()=> setNavToggle(false)} className="siderbar__links"><IoMdClose onClick={()=> setNavToggle(false)} className='sidebar__close'/></li>
           </ul>
             <span className="nav__routes"><AiOutlineBars onClick={() => setNavToggle(!navToggle)} className='navbar__bars'/></span>
         </div>
