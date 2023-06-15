c5064aa151a7a511a00a4053a3945191ffbdc608
AbbosxojaMarufxojayev <abbosxojamarufxojayev07@gmail.com>
2023-06-15 10:02:43 +0500

    navbar fix

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -10,17 +10,24 @@ import navLogo from "../../assets/navbar/navbarLogo.png"
 function Navbar() {
   const {pathname} = useLocation()
   const [navToggle, setNavToggle] = useState(false)
-  if(pathname === "/admin" || pathname === "/login"){
+  if(pathname.includes("admin") || pathname.includes("login")){
     return <></>
   }
 
+  document.body.style.overflow = navToggle ? "hidden" : "auto"
+
+
   return (
-    <div className='container'>
+    <div >
+       <div className={navToggle ? "container nav__wrapper" : "container"} onClick={() => {
+        setNavToggle(false)
+       }}>
+       </div>
        <div className="navbar">
        <div className="nav__logo">
           <NavLink to={"/"}><img src={navLogo} alt="" /></NavLink>
         </div>
-        <div className="navbar__collection">
+        <div className="navbar__collection" >
           <ul className={navToggle ? " nav__collection activeNav " : "nav__collection"}>
             <li onClick={()=> setNavToggle(false)} className="nav__routes"><NavLink to={"/"}>Asosiy sahifa</NavLink></li>
             <span className='cutter__navbar'>|</span>
